3eeec48006b2ace98686e16e8d2d97a92b4f4efd
gapgit <dlarkqrl4966@gmail.com>
2019-09-28 22:09:10 +0900

    Pick_View Edit

--- pick_comment/views.py
+++ pick_comment/views.py
@@ -13,58 +13,56 @@ class PickCommentView(View):
         offset = int(request.GET.get('offset', 0))
         limit  = int(request.GET.get('limit', 10))
 
-        comment_data = PickComment.objects.select_related('pick').filter(pick = pick_id, is_deleted = False).values()
+        comment_data = PickComment.objects.select_related('pick', 'user').filter(pick = pick_id, is_deleted = False)
         total_count  = comment_data.count()
         
         data = [{
-            'user_name'    : Accounts.objects.get(id = comment['user_id']).name,
-            'user_email'   : Accounts.objects.get(id = comment['user_id']).email,
+            'user_name'    : user.name,
+            'user_email'   : user.email,
             'pick_id'      : comment['pick_id'],
-            'pick_title'   : Pick.objects.get(pick_id = comment['pick_id']).title,
+            'pick_title'   : pick.title,
             'comment_id'   : comment['id'],
             'content'      : comment['content'],
-            } for comment in comment_data.order_by('created_at').reverse()[offset:limit]]
+            } for comment in comment_data.order_by('-created_at')[offset:limit]]
 
-        return JsonResponse({'total_count' : total_count, 'data' : data}, safe = False, status = 200)
+        return JsonResponse({'total_count' : total_count, 'data' : data})
 
-    @login_required
+@login_required
     def post(self, request, pick_id):
         data = json.loads(request.body)
+
+         if 'content' not in data or len(data['content']) == 0:
+            return JsonResponse({'ERROR':'COMMENT_MISSING'}, status = 400)
         
         try:
-            login_user = request.user
             pick_data = Pick.objects.get(pick_id = pick_id)
-        except Pick.DoesNotExist:
-            return JsonResponse({'message':'NO_PICK_IN_DB'}, status = 404)
 
-        if 'content' not in data or len(data['content']) == 0:
-            return JsonResponse({'message':'COMMENT_MISSING'}, status = 400)
-
-        PickComment.objects.create(
-            user    = login_user, 
-            pick    = pick_data,
-            content = data['content'],
-        )
+            PickComment.objects.create(
+                user    = login_user, 
+                pick    = pick_data,
+                content = data['content'],
+            )
         
-        return HttpResponse(status = 200)
-
+            return HttpResponse(status = 200)
+        except Pick.DoesNotExist:
+            return JsonResponse({'ERROR':'NOT_FOUND'}, status = 404)
+       
 class CommentEditingView(View):
     @login_required
     def post(self, request, pick_id, comment_id):
         data = json.loads(request.body)
-        
-        try:
-            user_comment = PickComment.objects.get(id = comment_id, pick_id = pick_id , user = request.user)
+       
+        if 'content' not in data or len(data['content']) == 0:
+            return JsonResponse({'ERROR':'COMMENT_MISSING'}, status = 400)
 
-            if 'content' not in data or len(data['content']) == 0:
-                return JsonResponse({'message':'COMMENT_MISSING'}, status = 400)
-            
+        try:
+            user_comment = PickComment.objects.get(id = comment_id, user = request.user)                       
             user_comment.content = data['content']
             user_comment.save()
 
             return HttpResponse(status = 200)
         except PickComment.DoesNotExist:
-            return JsonResponse({'message':'COMMENT_NOT_EXIST'}, status = 401)
+            return JsonResponse({'ERROR':'COMMENT_NOT_EXIST'}, status = 401)
 
     @login_required
     def delete(self, request, pick_id, comment_id):
@@ -75,4 +73,4 @@ class CommentEditingView(View):
 
             return HttpResponse(status = 200)
         except PickComment.DoesNotExist:
-            return JsonResponse({'message':'COMMENT_NOT_EXIST'}, status = 401)
+            return JsonResponse({'ERROR':'COMMENT_NOT_EXIST'}, status = 401)
